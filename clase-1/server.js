7f69d7d7b359d065c702ee05f1c19a97dccb3dba
Cristian Cortez <cortez.cristian@gmail.com>
2016-03-18 19:47:04 -0300

    Adding clase 1

---
+++ clase-1/server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Hola Mundo\n');
+}).listen(8080, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:8080/')
+//console.log(">>", process);
+
+/*
+setTimeout(function(){
+	process.exit();
+}, 5000)
+*/
